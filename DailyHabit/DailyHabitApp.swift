bb8bd7578ccab209dadc1ab89710dbfcb80e5c7a
Mirsait Sabirzyanov <mirsait77777@gmail.com>
2023-11-24 23:02:15 +0300

    Initial Commit

---
+++ DailyHabit/DailyHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyHabitApp.swift
+//  DailyHabit
+//
+//  Created by Мирсаит Сабирзянов on 24.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct DailyHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
